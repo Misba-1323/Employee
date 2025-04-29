448cb6023315f7f5f630d5beaf2cae9ac403b206
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-29 08:44:05 +0530

    save in main

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package Employee;
+
+public class Employee {
+   String name;
+   int id;
+   double salary;
+   //Constructors
+   public Employee(String name, int id, double salary) {
+    this.name = name;
+    this.id = id;
+    this.salary = salary;
+   }
+    //Method to display employee details
+    public void displayInfo(){
+
+        System.out.println("Employee ID: "+id);
+        System.out.println("Employee Name: "+name);
+        System.out.println("Salary: ₹"+salary);
+    }
+    //Main method
+    public static void main(String[] args) {
+        //Creating an object of Employee
+        Employee emp1 = new Employee("John", 101, 50000.0);
+        emp1.displayInfo();
+    }
+}
